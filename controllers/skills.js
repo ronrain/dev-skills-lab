6cac6e94bb48c2260ce3e61842d7ff2c84b5e355
RonRain <rarainero@gmail.com>
2023-09-12 22:07:53 -0400

    part 2 finish

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,10 +1,28 @@
-import { skills } from '../data/skill-data.js'
+import { Skill } from '../models/skill.js'
+// This ^^ takes the place of the import we were using previously, so we can delete it!
+// ~~import { todos } from '../data/todo-data.js'~~
 
+
+// function index(req.res) {
+//   res.render('todos/index', {
+
+//   })
+// }
+
+// Inside the index controller, use the Skill model to query for ALL skills
 function index(req, res){
-  res.render('skills/index', {
-    skills: skills
+  Skill.find({})
+  .then(skills => {  // skills represents the result of the query, in this case ALL skills
+    res.render('skills/index',{
+      skills: skills,
+    })
+  })
+  .catch(error => {  // If there's an error, console.log it and redirect back home!
+    console.log(error)
+    res.redirect('/')
   })
 }
+
 
 export {
   index
