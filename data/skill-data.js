4c9d7a2fa4325544ccabf7995a2492dca9ff3070
RonRain <rarainero@gmail.com>
2023-09-12 20:39:03 -0400

    finish part 1

---
+++ data/skill-data.js
@@ -0,0 +1,10 @@
+const skills = [
+  {text: 'lacrosse', active: true, play: false},
+  {text: 'physical fitness', active: true, play: true},
+  {text: 'video games', active: false, play: false},
+  {text: 'reading', active: false, play: true},
+]
+
+export {
+  skills
+}
---
+++ routes/skills.js
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+import * as skillsCtrl from '../controllers/skills.js'
+const router = Router()
+
+router.get('/', skillsCtrl.index)
+
+export { router }
